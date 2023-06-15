97dbdd03f6b36b0dc778084b6beb6d666fce8aa6
Etienne Soubes-Goldman <65122382+EtienneS-G@users.noreply.github.com>
2023-06-15 17:25:09 +0100

    creating initial website layout

---
+++ script.js
@@ -0,0 +1,29 @@
+// Creating the JS object to work from - NOTE prices are made up for testing//
+
+const dentists = [
+    {
+        name: "Taptonville House Dental Practice",
+        phoneNumber: "0114 399 0888",
+        address: "1 Taptonville Road, Broomhill, SHEFFIELD, S10 5BQ",
+        websiteURL: "https://taptonville.dentalpartners.co.uk/",
+        whitenPrice: 350,
+
+    },
+    {
+        name: "Bealby & Jones",
+        phoneNumber: "0114 272 9927",
+        address: "17 Northumberland Road, SHEFFIELD, S10 2TT",
+        websiteURL: "http://www.bealbyandjones.co.uk/",
+        whitenPrice: 350,
+
+    },
+    {
+        name: "Sharrow Vale Dental Care",
+        phoneNumber: "0114 268 6076",
+        address: "262 Sharrow Vale Road, Hunters Bar, SHEFFIELD, South Yorkshire, S11 8ZH",
+        websiteURL: "http://www.sharrowvaledentalcare.co.uk/",
+        whitenPrice: 450, 
+
+    }
+]
+
